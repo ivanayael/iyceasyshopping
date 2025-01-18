135d2cc4f0e0e4ae66af6b24c0511077b3f574b8
ivanayael <ivanyael@hotmail.com>
2025-01-18 20:55:27 -0300

    style, item, sales, ticket, coins, localization

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-let pedido = [];
+let sale = [];
 let total = 0;
 
-function agregarAlPedido(nombre, precio) {
+function addingSale(name, price) {
   // Agregar el producto al carrito
-  pedido.push({ nombre, precio });
+  sale.push({ name, price });
   
   // Actualizar el carrito en la interfaz
-  actualizarPedido();
+  updateSale();
 }
 
-function actualizarPedido() {
-  const listaPedido = document.getElementById('listaPedido');
+function updateSale() {
+  const listSale = document.getElementById('sale');
   const totalSpan = document.getElementById('total');
 
   // Limpiar la lista del carrito
-  listaPedido.innerHTML = '';
+  listSale.innerHTML = '';
 
   // Recorrer el carrito y agregar los productos a la lista
-  pedido.forEach(producto => {
+  sale.forEach(product => {
     const li = document.createElement('li');
-    li.textContent = `${producto.nombre} - $${producto.precio}`;
-    listaPedido.appendChild(li);
+    li.textContent = `${product.name} - $${product.price}`;
+    listSale.appendChild(li);
   });
 
   // Actualizar el total
-  total = pedido.reduce((acc, producto) => acc + producto.precio, 0);
+  total = sale.reduce((acc, sale) => acc + sale.price, 0);
   totalSpan.textContent = total;
 }
